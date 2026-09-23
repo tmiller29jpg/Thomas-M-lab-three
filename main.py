3cbb345e29023e517185e7b7ad40e303b4ad763e
buuuughhhhhhrp <tmiller29@setoncatholic.org>
2026-09-23 18:20:39 +0000

    donme

---
+++ main.py
@@ -0,0 +1,21 @@
+#function that adds 2 numbers
+def add (x,y):
+    print(x+y)
+
+#function that subtract 2 numbers
+def subtract (x,y):
+    print(x-y)
+
+#function that multiplies 2 numbers
+def multiply (x,y):
+    print(x*y)
+
+#function that divides 2 numbers 
+def divide (x,y):
+    print(x/y)
+
+x = int(input("enter the first number:"))
+y = int(input("enter the second number:"))
+
+add(x,y)
+print("welcome to Thomas's awesome calculator")

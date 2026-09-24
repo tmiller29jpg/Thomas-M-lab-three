6ae91944dc9df0a7f09ec96b8356f41ed735c18f
buuuughhhhhhrp <tmiller29@setoncatholic.org>
2026-09-24 21:10:45 +0000

    67

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+easter_egg_number = 67
+
 #function that adds 2 numbers
 def add (x,y):
+    if x + y == 67 or {x, y} == {2, 67}:
+        print(("67 " * easter_egg_number).strip())
+        return
     print(x+y)
 
 #function that subtract 2 numbers
@@ -19,3 +24,21 @@ y = int(input("enter the second number:"))
 
 add(x,y)
 print("welcome to Thomas's awesome calculator")
+while True:
+    print("what would you like to do?")
+    print("type (a)dd (s)ubtract (m)ultiply (d)ivide (q)uit")
+
+    user_choice=input(":")
+    #print(user_choice)
+
+    if user_choice == "a":
+        add(x,y)
+    elif user_choice == "s":
+        subtract(x,y)
+    elif user_choice == "m":
+        multiply(x,y)           
+    elif user_choice == "d":
+        divide(x,y) 
+    elif user_choice == "q":
+        print("goodbye")
+        break
